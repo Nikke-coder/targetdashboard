6636747430e50d2dc7729f04981fc9762b9f0f75
Nikke-coder <niklas.isaksson@targetflow.fi>
2026-03-21 16:06:40 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,13 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const URL = "https://nghlvfngpfrhhigkoeem.supabase.co"
-const KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5naGx2Zm5ncGZyaGhpZ2tvZWVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NDQyMTgsImV4cCI6MjA4OTIyMDIxOH0.Y4JF1x0m5BKa-btWkQAr1wY1EyTK30nck-hSL2eSNPE"
-
-export const supabase = createClient(URL, KEY, {
-  auth: {
-    detectSessionInUrl: true,
-    persistSession: true,
-    autoRefreshToken: true,
-  }
-})
-export const CLIENT = 'demo'
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
